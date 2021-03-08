4676c6f7d16f199426b6fea3e14da41661884d57
Nurmukhammed <nurekeee.013@gmail.com>
2021-03-09 04:45:29 +0600

    usercommit

--- src/com/company/data/IPostgresDB.java
+++
@@ -1,8 +0,0 @@
-package com.company.data;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface IPostgresDB {
-    Connection getConnection() throws SQLException,ClassNotFoundException;
-}
